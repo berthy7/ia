c9a53e311acaa53305b38123ee61e8c2d97e9215
berthy7 <berthy.v2@gmail.com>
2024-04-27 15:31:26 -0400

    se agrego nltk a index home

--- system/home/views.py
+++ system/home/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from system.chatBot.models import Intent,Pattern,Response
 # Create your views here.
-
+import nltk
 
 @login_required
 def home(request):
 
     obj= {'datos':{'lista':list(Intent.objects.all().values('id','tag'))}}
+    nltk.download('punkt')
+    nltk.download('wordnet')
+    nltk.download('omw-1.4')
 
     return render(request, 'home.html',obj)
 
